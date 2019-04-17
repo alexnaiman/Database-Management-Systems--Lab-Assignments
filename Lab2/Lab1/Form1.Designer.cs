1573587faae4bbaf752fa5fd22a12540d177ee31
Alexandru Naiman <alex.naiman.4@gmail.com>
2019-04-17 21:13:12 +0300

    Finished lab

---
+++ Lab2/Lab1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.dataGridViewChild = new System.Windows.Forms.DataGridView();
+            this.parentLabel = new System.Windows.Forms.Label();
+            this.childLabel = new System.Windows.Forms.Label();
+            this.viewAllButton = new System.Windows.Forms.Button();
+            this.saveChangesButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.AllowUserToAddRows = false;
+            this.dataGridViewParent.AllowUserToDeleteRows = false;
+            this.dataGridViewParent.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(12, 29);
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToFirstHeader;
+            this.dataGridViewParent.RowTemplate.Height = 24;
+            this.dataGridViewParent.Size = new System.Drawing.Size(356, 362);
+            this.dataGridViewParent.TabIndex = 0;
+            // 
+            // dataGridViewChild
+            // 
+            this.dataGridViewChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewChild.Location = new System.Drawing.Point(374, 29);
+            this.dataGridViewChild.Name = "dataGridViewChild";
+            this.dataGridViewChild.RowTemplate.Height = 24;
+            this.dataGridViewChild.Size = new System.Drawing.Size(553, 362);
+            this.dataGridViewChild.TabIndex = 1;
+            // 
+            // parentLabel
+            // 
+            this.parentLabel.AutoSize = true;
+            this.parentLabel.Location = new System.Drawing.Point(50, 9);
+            this.parentLabel.Name = "parentLabel";
+            this.parentLabel.Size = new System.Drawing.Size(136, 17);
+            this.parentLabel.TabIndex = 2;
+            this.parentLabel.Text = "Party Table - Parent";
+            // 
+            // childLabel
+            // 
+            this.childLabel.AutoSize = true;
+            this.childLabel.Location = new System.Drawing.Point(520, 9);
+            this.childLabel.Name = "childLabel";
+            this.childLabel.Size = new System.Drawing.Size(155, 17);
+            this.childLabel.TabIndex = 3;
+            this.childLabel.Text = "Character Sheet - Child";
+            // 
+            // viewAllButton
+            // 
+            this.viewAllButton.Location = new System.Drawing.Point(53, 397);
+            this.viewAllButton.Name = "viewAllButton";
+            this.viewAllButton.Size = new System.Drawing.Size(133, 41);
+            this.viewAllButton.TabIndex = 4;
+            this.viewAllButton.Text = "View All";
+            this.viewAllButton.UseVisualStyleBackColor = true;
+            this.viewAllButton.Click += new System.EventHandler(this.viewAllButton_Click);
+            // 
+            // saveChangesButton
+            // 
+            this.saveChangesButton.Location = new System.Drawing.Point(523, 397);
+            this.saveChangesButton.Name = "saveChangesButton";
+            this.saveChangesButton.Size = new System.Drawing.Size(243, 40);
+            this.saveChangesButton.TabIndex = 5;
+            this.saveChangesButton.Text = "Save All Changes";
+            this.saveChangesButton.UseVisualStyleBackColor = true;
+            this.saveChangesButton.Click += new System.EventHandler(this.saveChangesButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 450);
+            this.Controls.Add(this.saveChangesButton);
+            this.Controls.Add(this.viewAllButton);
+            this.Controls.Add(this.childLabel);
+            this.Controls.Add(this.parentLabel);
+            this.Controls.Add(this.dataGridViewChild);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewParent;
+        private System.Windows.Forms.DataGridView dataGridViewChild;
+        private System.Windows.Forms.Label parentLabel;
+        private System.Windows.Forms.Label childLabel;
+        private System.Windows.Forms.Button viewAllButton;
+        private System.Windows.Forms.Button saveChangesButton;
+    }
+}
+
---
+++ Lab2/Lab1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Collections.Specialized;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection = new SqlConnection(GetString());
+        private SqlDataAdapter parentDataAdapter;
+        private SqlDataAdapter childDataAdapter;
+        private SqlCommandBuilder commandBuilder;
+        private DataSet parentDataSet = new DataSet();
+        private DataSet childDataSet = new DataSet();
+
+        private static string GetString()
+        {
+             
+            return ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString.ToString();
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridViewParent.SelectionChanged += new EventHandler(LoadSelectedCharacters);
+            LoadParentTable();
+        }
+        
+        private void LoadParentTable()
+        {
+            parentDataAdapter = new SqlDataAdapter(ConfigurationManager.AppSettings.Get("SelectParent"), connection);
+            parentDataAdapter.Fill(parentDataSet, ConfigurationManager.AppSettings.Get("ParentTableName"));
+            dataGridViewParent.DataSource = parentDataSet.Tables[ConfigurationManager.AppSettings.Get("ParentTableName")];
+        }
+
+        private void LoadSelectedCharacters(object sender, EventArgs e)
+        {
+            int parentId = Convert.ToInt32(dataGridViewParent.CurrentRow.Cells[0].Value);
+            SqlCommand command = new SqlCommand(ConfigurationManager.AppSettings.Get("SelectChild"))
+            {
+                Connection = new SqlConnection(GetString())
+            };
+
+            command.Parameters.AddWithValue(ConfigurationManager.AppSettings.Get("ParentColumn"), parentId);
+
+            childDataAdapter = new SqlDataAdapter(command);
+            childDataSet = new DataSet();
+            childDataAdapter.Fill(childDataSet, ConfigurationManager.AppSettings.Get("ChildTableName"));
+            dataGridViewChild.DataSource = childDataSet.Tables[ConfigurationManager.AppSettings.Get("ChildTableName")];
+        }
+
+        private void viewAllButton_Click(object sender, EventArgs e)
+        {
+            childDataAdapter = new SqlDataAdapter(ConfigurationManager.AppSettings.Get("SelectAllChild"), GetString());
+            childDataSet = new DataSet();
+            childDataAdapter.Fill(childDataSet, ConfigurationManager.AppSettings.Get("ChildTableName"));
+            dataGridViewChild.DataSource = childDataSet.Tables[ConfigurationManager.AppSettings.Get("ChildTableName")];
+        }
+
+        private void saveChangesButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                commandBuilder = new SqlCommandBuilder(childDataAdapter);
+                childDataAdapter.Update(childDataSet, ConfigurationManager.AppSettings.Get("ChildTableName"));
+                MessageBox.Show("Updated Successfully");
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Something went wrong :(" + ex.ToString());
+            }
+        }
+    }
+}
